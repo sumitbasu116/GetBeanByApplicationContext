fb46d7bec3218af4faf323edff3dd206ce3dd6a8
Sumit Basu <sumit@bytemark.co>
2021-03-21 11:27:57 +0530

    applicationContext-real-usecase

---
+++ src/main/java/com/sumit/service/MyApplicationService.java
@@ -0,0 +1,11 @@
+package com.sumit.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyApplicationService {
+
+	public String getMyMessage() {
+		return "This is my Area!";
+	}
+}
